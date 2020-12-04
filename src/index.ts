e50e41ee40d77a1a5757ade23ea369b8b5dbd552
Maxi Scalzotto <maxiscalzotto@gmail.com>
2020-12-04 15:29:45 -0300

    adding params in response

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.use(`${basePath}/api`, api);
 
+app.use(`/`, (_req, res) => {
+  res.send('<h1>Welcome to prontoform-test project!');
+});
+
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
