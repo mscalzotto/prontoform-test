15f919611e88c27cfc6ef3754fe018ee3f1719e0
Maxi Scalzotto <maxiscalzotto@gmail.com>
2020-12-04 15:11:27 -0300

    change import

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,8 @@
 import express, { Request, Response } from 'express';
 
-const api: express.Application = express();
+export const api: express.Application = express();
 api.use(express.json());
 
 api.post('/inspection/create', (req: Request, res: Response) => {
-  res.json({requestBody: req.body});
+  res.json({ requestBody: req.body });
 });
-
-export default api;
