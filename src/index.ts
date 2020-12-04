74ab836de098c968bdcf2cdf7372e87588a6a803
Maxi Scalzotto <maxiscalzotto@gmail.com>
2020-12-04 15:39:43 -0300

    adding params in response

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import { api } from './api';
 
 const app = express();
 
-const basePath = '/prontoform-test';
+const basePath = '/prontoform';
 const port: string | number = process.env.PORT || 5000;
 
 app.use(express.json());
+
+const healthcheck = require('express-healthcheck')(); // tslint:disable-line
+app.get(`${basePath}/health(check)?`, healthcheck);
 
 app.use(`${basePath}/api`, api);
 
