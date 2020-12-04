91cf4c2c4f1caa5eafd08715b0f68d236d405d5f
Maxi Scalzotto <maxiscalzotto@gmail.com>
2020-12-04 10:11:09 -0300

    minor modifications to deploy properly on herolu

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express from 'express';
-import http from 'http';
 import api from './api';
 
 const app = express();
 
 const basePath = '/prontoform-test';
+const port: string | number = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use('*', (_req, res) => {
+  res.send('<h1>Welcome to your server!</h1>');
+});
+
 app.use(`${basePath}/api`, api);
 
-const server = http.createServer(app);
-
-server.listen(3000, () => {
-  console.log("Listening on port 3000")
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
