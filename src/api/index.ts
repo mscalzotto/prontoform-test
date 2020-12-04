5a86c2332ce0a5c201beebb4c01475ee81fa92ce
Maxi Scalzotto <maxiscalzotto@gmail.com>
2020-12-04 17:20:56 -0300

    added node-cache service

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,13 @@
 import express, { NextFunction, Request, Response } from 'express';
+import NodeCache from 'node-cache';
+
+const DEFAULT_TTL = 172800;
+
+const cacheService: NodeCache = new NodeCache({
+  stdTTL: DEFAULT_TTL,
+  checkperiod: DEFAULT_TTL * 0.2,
+  useClones: false,
+});
 
 export const api: express.Application = express();
 api.use(express.json());
@@ -10,12 +19,26 @@ api.use('/', (req: Request, res: Response, next: NextFunction) => {
   ) {
     next();
   }
+  res.statusCode = 401;
+  res.send('<h1>Unauthorized!</h1>');
+});
 
-  res.status(401).send('<h1>Unauthorized!</h1>');
+api.get('/cache/inspection', (_req: Request, res: Response) => {
+  res.json(cacheService.keys());
+});
+
+api.get('/cache/inspection/:id', (req: Request, res: Response) => {
+  res.json(cacheService.get(req.params.id));
 });
 
 api.post('/inspection/create', (req: Request, res: Response) => {
-  console.info('[REQUEST BODY]', JSON.stringify(req.body, null, 2));
+  cacheService.set(
+    `${parseInt(`${Math.random() * 1000000}`)}`,
+    req.body,
+    DEFAULT_TTL
+  );
+
+  res.statusCode = 201;
   res.json({
     requestBody: req.body,
   });
