d68c8b2c17391b064773103e06f067574f743e16
Maxi Scalzotto <maxiscalzotto@gmail.com>
2020-12-04 16:08:08 -0300

    Adding some security

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,21 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 
 export const api: express.Application = express();
 api.use(express.json());
 
+api.use('/', (req: Request, res: Response, next: NextFunction) => {
+  if (
+    req.headers.hasOwnProperty('authorization') &&
+    req.headers['authorization'] === 'Bearer 123456'
+  ) {
+    next();
+  }
+
+  res.status(401).send('<h1>Unauthorized!</h1>');
+});
+
 api.post('/inspection/create', (req: Request, res: Response) => {
-  res.json({
+  res.status(201).json({
     requestBody: req.body,
-    headers: req.headers,
   });
 });
